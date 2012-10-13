9351ed5e559b19a342bec554596c0166aae021c2
Biko Tushinde <biko@smashingboxes.com>
2012-10-12 20:37:52 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+War::Application.config.secret_token = '4032766f337ccc36e9a6efdbc1972801a4163f1ceb124f1e784e174db2d6add2d83fd8d55e83161827c7c11f689c8aced6720392939e3ab20d468b40631b02cb'
